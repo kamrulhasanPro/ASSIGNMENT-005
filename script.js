c6feee4045b32a9a57ba6e048734a5bff5cd2621
kamrulmiazi <kamrul116522@gmail.com>
2025-08-28 00:30:23 +0600

    call alert and coin setup compleate

--- script.js
+++ script.js
@@ -18,10 +18,10 @@ makeId('products-section').addEventListener('click', (event)=>{
     
     // copy count and alert and copy number in clipboard
     if(event.target.id === 'copy-btn' || event.target.parentNode.id === 'copy-btn'){
-
+        
         // alert
         alert('The number is copy')
-
+        
         // copy number
         if(event.target.id === 'copy-btn'){
             const copyNumber = event.target.parentNode.parentNode.children[2].innerText.trim();
@@ -38,7 +38,41 @@ makeId('products-section').addEventListener('click', (event)=>{
             i.innerText = Number(i.innerText) + 1;
         }
     }    
-
     
+    
+    // call button click 
+    const callBtn = event.target.id === 'call-btn';
+    const callIconParent = event.target.parentNode.id === 'call-btn'; 
+    if( callBtn||callIconParent ){
+        
+        
+        // coin 
+        let coins = document.getElementsByClassName('coin')
+        // console.log(coins);
+        for(let coin of coins){
+            // console.log(coin.innerText);
+            if(Number(coin.innerText) < 20){
+                    alert('❌ else not allowed because your coin not enough')
+                    return
+                }
+            
+            coin.innerText = Number(coin.innerText) - 20;
+        }
+        let title = event.target.parentNode.parentNode.parentNode.children[0].innerText;
+        
+        let number = event.target.parentNode.parentNode.parentNode.children[2];
+        if(callIconParent){
+            alert(`📞 This title ${title} and this number ${number.innerText}`)
+        }
+        else if(callBtn){
+            title = event.target.parentNode.parentNode.children[0].innerText;
+            number = event.target.parentNode.parentNode.children[2].innerText
+            
+            alert(`📞 This title ${title} and this number ${number}`)
+        }
+        
 
+
+
+    }
 });
